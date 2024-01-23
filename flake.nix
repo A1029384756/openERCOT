d95437c95d35f1466bd466192cc6c3bdc041d9ea
Hayden Gray <hayden.gray104@gmail.com>
2024-01-23 14:04:27 -0500

    nix flake added highspy

--- flake.nix
+++ flake.nix
@@ -50,6 +50,29 @@
           propagatedBuildInputs = commonArgs;
         };
 
+        highspy = pkgs.python311Packages.buildPythonPackage rec {
+          pname = "highspy";
+          version = "1.5.3";
+          format = "wheel";
+
+          src =
+            let
+              computePath = system: (
+                if system == "x86_64-linux" then
+                  "manylinux_2_17_x86_64.manylinux2014_x86_64"
+                else if system == "x86_64-darwin" then
+                  "macosx_10_9_x86_64"
+                else if system == "aarch64-darwin" then
+                  "macosx_11_0_arm64"
+                else throw "Unsupported system: ${system}"
+              );
+            in
+            builtins.fetchurl {
+              url = "https://files.pythonhosted.org/packages/31/65/41e1b4774a999bf72301ca4146bc5050cd803ef46dfd7bcc12a3da192cbb/highspy-1.5.3-cp311-cp311-${computePath(system)}.whl";
+              sha256 = "sha256:0ya5ydxkxan19a8q16hz766b131wjh7dqiicfwdmkfv1hcchxk61";
+            };
+        };
+
         pypsa = pkgs.python311Packages.buildPythonPackage rec {
           pname = "pypsa";
           version = "0.26.2";
@@ -60,7 +83,7 @@
             hash = "sha256-uq/ZAF9InBNU4HBKKTLZPZJUyxBoDet70cIkCOCvw9w=";
           };
 
-          propagatedBuildInputs = commonArgs ++ [ linopy ];
+          propagatedBuildInputs = commonArgs ++ [ linopy highspy ];
         };
 
         pyEnv = pkgs.python311.withPackages (ps:
