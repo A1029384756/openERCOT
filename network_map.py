30c624b52904c4f76e02ecf1a223884e486122b5
Malcolm Moss <promalcolm@gmail.com>
2024-02-29 12:26:37 -0500

    added better colors and nodal plotting

--- network_map.py
+++ network_map.py
@@ -36,30 +36,49 @@ def draw_map_cartopy(ax, geomap=True, color_geomap=None):
 
     ax.add_feature(
         cartopy.feature.BORDERS.with_scale(resolution),
-        linewidth=0.3,
+        linewidth=0.7,
         color=color_geomap.get("border", "k"),
     )
 
     ax.add_feature(
         cartopy.feature.STATES.with_scale(resolution),
-        linewidth=0.2,
+        linewidth=0.7,
         edgecolor=color_geomap.get("state", "k"),
     )
 
     ax.add_feature(
         cartopy.feature.COASTLINE.with_scale(resolution),
-        linewidth=0.3,
+        linewidth=0.7,
         color=color_geomap.get("coastline", "k"),
     )
 
 
-fig, ax = plt.subplots(subplot_kw={"projection": ccrs.PlateCarree()})
-draw_map_cartopy(ax, True, True)
+if __name__ == "__main__":
+    fig, ax = plt.subplots(subplot_kw={"projection": ccrs.PlateCarree()})
+    draw_map_cartopy(ax, True, True)
 
-network = pypsa.Network()
-network.import_from_netcdf(path="network.nc")
-network.plot(
-    geomap=False,
-    margin=0.5,
-)
-plt.show()
+    network = pypsa.Network()
+    network.import_from_netcdf(path="network.nc")
+    gen = network.generators.groupby(["bus", "carrier"]).p_nom.sum()
+
+    network.plot(
+        title="Open ERCOT Total Nodal Generation Capacity",
+        geomap=False,
+        bus_sizes=gen / 5e4,
+        bus_colors={"dfo": "#c6a0f6",
+                    "coal": "#ee99a0",
+                    "gas": "#f5a97f",
+                    "nuclear": "#a6da95",
+                    "biomass": "#91d7e3",
+                    "solar": "#8aadf4",
+                    "wind": "#f0c6c6",
+                    "other": "#7dc4e4",
+                    "hydro": "#b7bdf8"},
+        link_widths=2,
+        margin=.2,
+        link_colors="black",
+        color_geomap=True,
+    )
+
+    plt.tight_layout()
+    plt.show()
